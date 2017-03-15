ad4c511de358a2c9fc67d00cfa35b621b7860ef9
pavelmacu <pavel.alfredo.macutela@estudiant.upc.edu>
2017-03-15 09:45:11 +0100

    agregado la gestion de plazas libres

--- P2/parking.sql
+++ P2/parking.sql
@@ -11,28 +11,28 @@ CREATE TABLE PK(
 
 
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (23 , '1234ASD', 'Red','Opel', strftime('%s', 'now'));
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (0 , '1234ASQ', 'Red', 'Ferrari', strftime('%s','now'));
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (2 , '1234ASE', 'Blue', 'Tata', strftime('%s','now'));
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (3 , '1234ASU', 'Blue', 'Toyota', strftime('%s','now'));
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (1 , '1234ASI', 'Black', 'Opel', strftime('%s','now'));
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (6 , '1234ASK', 'White', 'Renault', strftime('%s','now'));
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (25 , '1234ASL', 'White', 'Honda', strftime('%s','now'));
 
-INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA) 
+INSERT INTO PK (PLACE,NP,COLOR,BRAND, DATA)
 VALUES (22 , '1234ASB', 'Blue', 'Honda', strftime('%s','now'));
 
 
@@ -41,3 +41,6 @@ select strftime('%s','now')-DATA from PK where PLACE = '0';
 
 /*Retorna els dies*/
 select (strftime('%s','now')-DATA)/3600/24 from PK where PLACE = '0';
+
+/*gestiona las plazas vacias*/
+select (30 - COUNT (*) )As plaza_vacias  from PK where place  NOT NULL;
